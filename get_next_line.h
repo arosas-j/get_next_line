992462c527b711957e486b73874eec5fdceff989
Alejandro Rosas Jaime <arosas-j@c3r2s6.42malaga.com>
2025-07-23 15:02:16 +0200

    Finished

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arosas-j <arosas-j@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 12:43:13 by arosas-j          #+#    #+#             */
+/*   Updated: 2023/10/24 17:46:43 by arosas-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*read_to_line(int fd, char *saved_line);
+char	*copy_to_line(char *saved_line);
+char	*fix_saved_line(char *saved_line);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+
+#endif
